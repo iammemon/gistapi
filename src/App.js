727cf207fbeb3765222c77b343263e4c3e780a47
Fawad Memon <meetfawad.online@gmail.com>
2021-02-19 11:16:29 +0500

    use composition for Header comp for easy prop passing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-
 import styled from 'styled-components'
 import Header from "./components/Header";
 import GlobalStyles from "./GlobalStyle";
+import Search from './components/Search'
+import { useState, useEffect } from 'react';
+import { getGistForUser, getPublicGists } from './services/gistService'
+
 
 const App = () => {
+
+  const [gists, setGists] = useState([])
+  const [err, setErr] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    fetchGists()
+  }, [])
+
+  const fetchGists = async (username = '') => {
+    let response;
+
+    try {
+      if (username) {
+        response = await getGistForUser(username)
+      } else {
+        response = await getPublicGists()
+      }
+      setGists(response.data)
+
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
   return (
     <Wrapper className="App" data-testid="app">
-      <Header />
+      <Header>
+        <Search fetchCallback={fetchGists} />
+      </Header>
       <GlobalStyles />
     </Wrapper>
   );
